618c5191afd29fc11a0ba030ac3267f0d0ac37dc
Joao Paulo <oliveirajpufrj@gmail.com>
2022-05-11 19:08:04 -0300

    feat: tela de cadastro

--- src/components/SignUp/style.js
+++ src/components/SignUp/style.js
@@ -1,4 +1,50 @@
 import styled from 'styled-components';
 
 export const $SignUp = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+margin-top: 50px;
+input{
+    width: 100vw;
+    max-width: 326px;
+    height: 58px;
+    background-color: #FFFFFF;
+    border-radius: 5px;
+    font-size: 20px;
+    color: #000000;
+    padding-left: 15px;
+}
+button{
+    width: 100vw;
+    max-width: 326px;
+    height: 46px;
+    background: #000;
+    border-radius: 5px;
+    font-size: 20px;
+    font-weight: 700;
+    color: #FFFFFF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 13px;
+    margin-bottom: 36px;
+}
+span{
+    font-weight: 700;
+    font-size: 15px;
+    color: #000;
+}
+img{
+    width: 120px;
+    border-radius: 50%;
+    margin-bottom: 50px;
+}
 `;
